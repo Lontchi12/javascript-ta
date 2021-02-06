ea7d7deab69dd68b559c15b175e6e8721826c774
Lontchi12 <lontchilionelle@gmail.com>
2021-02-06 16:47:21 +0100

    Added my own solution to palindrome assessment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
 // complete the function
-function palindrom(str) {
-  // code goes here
-}
+          function palindrom(str) {
+            let reg = /[\W_]/g;  // Regular expression to check for non alphanumeric characters in "str"
+                // This line converts "str" to lowercase and replaces it 
+            let string = str.toLowerCase().replace (reg, "");
+
+            let reversed = string.split("").reverse().join(""); 
+                   // This line splits, reverses and join  "str" 
+            if (reversed == string) {     // Checks if "str" is thesame when read backwards
+                return true;
+            } else {
+                return false;
+            }
+          }
+  
 
 function solution(arg) {
   return palindrom(arg);
